1350ac2a43aaab7fccefe6878468c24300452bbd
yeaseoPark <lim222ok@hufs.ac.kr>
2022-09-28 13:04:44 +0900

    오류 수정 및 info 레벨 로거 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -2,10 +2,11 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from pybo.models import Question
-
+import logging
+logger = logging.getLogger('pybo')
 
 def index(request):
-    3/0
+    logger.info("INFO 레벨로 출력")
     page = request.GET.get('page','1')
     kw= request.GET.get('kw','')
 
